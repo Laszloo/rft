0aac40c1001c774f6d6ce38c1eba045f5ca9204f
Elek László <e.laszlo96@gmail.com>
2025-11-12 16:22:33 +0100

    #10; ✨ feat: app\Routes\web.php

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use App\Http\Controllers\HomeController;
 use Slim\App;
 use App\Http\Controllers\DemoController;
 
 return function (App $app) {
-    $app->get('/', [DemoController::class, 'index']);
+    $app->get('/', [HomeController::class, 'index']);
+    $app->get('/demo', [DemoController::class, 'index']);
 };

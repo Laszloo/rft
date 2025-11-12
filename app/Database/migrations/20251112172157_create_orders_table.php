850914e54c0a9a5787d848718c3d2d4475a6e00d
Elek László <e.laszlo96@gmail.com>
2025-11-12 19:03:42 +0100

    #16; ✨ feat: created orders & orders item migration

---
+++ app/Database/migrations/20251112172157_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateOrdersTable extends AbstractMigration
+{
+    public function up(): void
+    {
+        if ($this->hasTable('orders')) {
+            $this->table('orders')->drop()->save();
+        }
+
+        $t = $this->table('orders', ['id' => false, 'primary_key' => ['id']]);
+        $t->addColumn('id', 'integer', ['identity' => true, 'signed' => false])
+            ->addColumn('user_id', 'integer', ['signed' => false])
+            ->addColumn('order_number', 'string', ['limit' => 32])
+            ->addColumn('status', 'enum', [
+                'values'  => ['folyamatban', 'fizetve', 'szallitva', 'teljesitve', 'torolve'],
+                'default' => 'folyamatban',
+            ])
+            ->addColumn('total_gross', 'decimal', ['precision' => 10, 'scale' => 2, 'default' => 0])
+            ->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'datetime', ['null' => true, 'default' => null])
+
+            ->addIndex(['order_number'], ['unique' => true, 'name' => 'uniq_order_number'])
+
+            ->create();
+
+        $this->table('orders')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                ['delete' => 'SET_NULL', 'update' => 'CASCADE', 'constraint' => 'fk_orders_user']
+            )
+            ->update();
+    }
+
+    public function down(): void
+    {
+        if ($this->hasTable('orders')) {
+            $this->table('orders')->drop()->save();
+        }
+    }
+}
---
+++ app/Database/migrations/20251112172743_create_orders_item_table.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateOrdersItemTable extends AbstractMigration
+{
+    public function up(): void
+    {
+        if ($this->hasTable('order_items')) {
+            $this->table('order_items')->drop()->save();
+        }
+
+        $this->table('order_items')
+            ->addColumn('order_id', 'integer', ['signed' => false])
+            ->addColumn('book_id', 'integer', ['signed' => false])
+            ->addColumn('qty', 'integer', ['default' => 1])
+            ->addColumn('unit_price', 'decimal', ['precision' => 10, 'scale' => 2])
+            ->addColumn('line_total', 'decimal', ['precision' => 10, 'scale' => 2])
+
+            ->addIndex(['order_id', 'book_id'], ['unique' => true, 'name' => 'uniq_order_item'])
+
+            ->create();
+
+        $this->table('order_items')
+            ->addForeignKey(
+                'order_id',
+                'orders',
+                'id',
+                ['delete' => 'RESTRICT', 'update' => 'CASCADE', 'constraint' => 'fk_oi_order']
+            )
+            ->addForeignKey(
+                'book_id',
+                'books',
+                'id',
+                ['delete' => 'RESTRICT', 'update' => 'CASCADE', 'constraint' => 'fk_oi_book']
+            )
+            ->update();
+    }
+
+    public function down(): void
+    {
+        if ($this->hasTable('order_items')) {
+            $this->table('order_items')->drop()->save();
+        }
+    }
+}

4a75759d0cc4d8e9aa4fb59607a445f721fc75b5
Elek László <e.laszlo96@gmail.com>
2025-11-13 11:23:53 +0100

    #20; ✨ feat: update routes

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 use App\Http\Controllers\HomeController;
 use Slim\App;
 use App\Http\Controllers\DemoController;
+use App\Http\Controllers\CartController;
 
 return function (App $app) {
+     $app->add(function ($req, $handler) {
+        if (session_status() === PHP_SESSION_NONE) { session_start(); }
+        return $handler->handle($req);
+    });
     $app->get('/', [HomeController::class, 'index']);
     $app->get('/demo', [DemoController::class, 'index']);
+
+    //  Kosár
+    $app->post('/kosar/hozzaad/{id}', [CartController::class, 'add']);
+    $app->get('/kosar', [CartController::class, 'view']);
+    $app->post('/kosar/frissit', [CartController::class, 'update']);
+    $app->get('/fizetes', [CartController::class, 'checkout']);
 };

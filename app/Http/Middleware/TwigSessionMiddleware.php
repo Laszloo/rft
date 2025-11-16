552d559b3e6467b0f1e974c9d40907e4d74926c7
Elek László <e.laszlo96@gmail.com>
2025-11-16 10:45:01 +0100

    #32 ✨ feat: create message view

--- app/Http/Middleware/TwigSessionMiddleware.php
+++ app/Http/Middleware/TwigSessionMiddleware.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
+use App\Config\Application;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Slim\Views\Twig;
@@ -18,6 +19,18 @@ final class TwigSessionMiddleware implements MiddlewareInterface
         $this->twig->getEnvironment()->addGlobal('app', $_SESSION);
         $this->twig->getEnvironment()->addGlobal('user', $_SESSION['user'] ?? null);
 
+        $this->messageCheck();
+
         return $handler->handle($request);
+    }
+
+    private function messageCheck(): void {
+        if (isset($_SESSION['messages'])) {
+            $_SESSION['messages'] = $_SESSION['messages'] = array_filter(
+                $_SESSION['messages'],
+                fn (array $message) =>
+                    $message['time'] + Application::MESSAGES_SHOW_TIME > time()
+            );
+        }
     }
 }
